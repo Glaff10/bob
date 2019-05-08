6874c8a61bc06108b1e4c83c1f444443df35eb00
Rodrigo Guimarães Queiroz <rodr.gq@gmail.com>
2019-05-08 16:55:16 -0400

    Add files via upload

---
+++ questao4.c
@@ -0,0 +1,77 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "binaria.h"
+#include "avl.h"
+#include "vetor.h"
+
+int main(){
+
+
+    int cont;
+    int *vet = criarVetor(1000000);
+    int *vetbusca = criarVetor(30);
+    preencheVetor(vetbusca,30);
+    
+    ArvoreBinaria *tree = inicializa();
+    avl *arvore = criaAvl();
+
+    int i,x;
+    avl *aux;
+    ArvoreBinaria *aux2;
+    clock_t ticks[2];
+    double tempoV, tempoA;
+
+    for(i=0;i<10;i++){
+        double tempoa, tempob;
+        
+        ArvoreBinaria *tree = inicializa();
+        avl *arvore;
+        preencheVetor(vet,10000);
+
+        ticks[0] = clock();
+
+
+        tree = preencheComVetor(tree,vet,10000);
+
+        ticks[1] = clock();
+        tempoa = (double) (ticks[1] - ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+
+        printf("%d execucao, criando no arvore binaria em %lf\n", i, tempoa);
+        //printf("%d",altura(tree));
+        ticks[0] = clock();
+        for(cont=0;cont<10000;cont++){
+        printf("dando lingua\n");
+        arvore = inserirAvl(arvore,vet[cont]);
+        }
+        ticks[1] = clock();
+        tempob = (double) (ticks[1] - ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+        printf("%d execucao, criando na arvore AVL em %lf\n", i, tempob);
+        printf("%d",arvore->altura);
+    }
+
+    for(i=0;i<30;i++){
+        ticks[0] = clock();
+        aux2 = busca(tree,vetbusca[i]);
+        if(aux2!=NULL) printf("Achou!\n");
+        else printf("Não achou!\n");
+        ticks[1] = clock();
+        tempoA = (ticks[1] - ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+        printf("%d° execucao, procurando %d na arvore binaria em %lf", i, vetbusca[i], tempoV);
+        ticks[0] = clock();
+        aux = buscaAvl(arvore,vetbusca[i]);
+        if(aux!=NULL) printf("Achou!\n");
+        else printf("Não achou!\n");
+        ticks[1] = clock();
+        tempoV = (ticks[1] - ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+        printf("%d° execucao, procurando %d na arvore AVL em %lf", i, vetbusca[i], tempoA);
+    }
+
+    tempoV /= 30;
+    tempoA /= 30;
+
+    printf("tempo gasto em media no vetor foi: %.1lf segundos\n", tempoV);
+    printf("tempo gasto em media na arvore foi: %.1lf segundos\n", tempoA);
+
+    return 1;
+}
