6082cd6ff877f746d9c129932d1b917b920c9607
Rodrigo Guimarães Queiroz <rodr.gq@gmail.com>
2019-06-24 09:18:48 -0400

    Add files via upload

---
+++ grafo/grafo.c
@@ -0,0 +1,82 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "stdbool.h"
+#include "grafo.h"
+#include "string.h"
+
+typedef struct Grafo grafo;
+
+*Grafo preencher_grafo(Grafo *g, int vertices, int arestas){
+	
+	g = (Grafo*) malloc(sizeof(Grafo));
+
+	srand(time(NULL));
+	valor = rand() % vertices;
+
+	memset(g->matriz, -1, sizeof(g->matriz)); // setado todos os espaços dessa matriz para -1
+	memset(g->visitado, 0, sizeof(g->visitado));
+
+	for(int i=0; i<vertices; i++){
+		for(int j=0; j<arestas; j++){
+			g->matriz[i][valor] = 1;
+			valor = rand() % vertices;
+		}	
+	}
+
+	return g;
+
+}
+
+void bfs(int v, int tam, Grafo *g){
+
+	queue fila; //TAD de fila
+	// no caso aqui seria fila *queue  = criar_fila(queue); algo assim
+
+	fila.push(v); //inserir
+	g->visitado[v] = 1; 
+
+	while(!fila.empty()){
+
+		int atual = fila.top(); // topo
+		fila.pop(); // retira da fila
+
+		for(int i=0; i<tam; i++){
+			if(g->visitado[matriz[v][i]] == -1 && g->matriz[v][i]){
+				fila.push(g->matriz[v][i]);
+				g->visitado[g->matriz[v][i]] = 1;
+			}
+		}
+	}
+}
+
+void dfs(int v, int tam, Grafo *g){
+
+	for(int i=0; i<tam; i++){
+
+		if(g->visitado[g->matriz[v][i]]==-1 && g->matriz[v][i]){
+			
+			int vizinho = g->matriz[v][i];
+
+			g->visitado[g->matriz[v][i]] = 1;
+			dfs(vizinho);
+		}
+	}
+}
+
+void dfs_todos_os_caminhos(int v, int tam, Grafo *g){
+
+	for(int i=0; i<tam; i++){
+
+		if(g->visitado[g->matriz[v][i]]==-1 && g->matriz[v][i]){
+			
+			int vizinho = g->matriz[v][i];
+
+			g->visitado[g->matriz[v][i]] = 1;
+			//acho que tem que printar aqui o caminho, vamo ver
+			dfs(vizinho);
+			g->visitado[g->matriz[v][i]] = -1;
+
+		}
+	}
+}
---
+++ grafo/grafo.h
@@ -0,0 +1,18 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+#define MAX 10
+
+struct Grafo{
+	
+	int matriz[MAX][MAX];
+	int visitado[MAX];
+};
+typedef struct Grafo grafo;
+
+//funcoes
+
+*Grafo preencher_grafo(Grafo *g, int vertices, int arestas);
+void bfs(int v, int tam, Grafo *g);
+void dfs(int v, int tam, Grafo *g);
+void dfs_todos_os_caminhos(int v, int tam, Grafo *g);
